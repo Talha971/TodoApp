be244dd491a8c97a598fb2806e9f9f8724525b4e
Talha971 <talhatahir971@gmail.com>
2025-03-20 20:19:53 +0500

    Add project files.

---
+++ WebApi-EF/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApi_EF.Models
+{
+    public class Event
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public DateTime Start { get; set; }
+        public DateTime? End { get; set; }
+        public bool AllDay { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
---
+++ WebApi-EF/Services/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi_EF.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
